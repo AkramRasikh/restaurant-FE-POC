d5e81dc08716c1a87e0a32ad40473085976a00bf
akram rasikh <akram.rasikh@gmail.com>
2021-02-22 09:09:38 +0000

    jest mock working correctly

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -4,19 +4,13 @@ import Home from '../../pages';
 import {attributes as frontmatter} from '../../content/content-markdown/home.md';
 import renderWithProviders from '../../test-utils/render-with-providers';
 import {socketMessages} from '../../utils/socket';
+import useSocket from '../../providers/socket-provider/use-socket';
 
-jest.mock('../../providers/socket-provider/use-socket', () => () => ({
-  emit: () => {},
-  orders: {
-    basket: [{id: 1, name: 'burger', price: 200}],
-  },
-}));
+jest.mock('../../providers/socket-provider/use-socket');
 
-jest.mock('socket.io-client', () => () => ({
-  on: () => {},
-  emit: () => {},
-}));
-
+afterEach(() => {
+  jest.clearAllMocks();
+});
 const renderComponent = (configProps = {}) => {
   return renderWithProviders({
     page: <Home data={{markdownRemark: {frontmatter}}} />,
@@ -25,12 +19,24 @@ const renderComponent = (configProps = {}) => {
 };
 // mockReturnValue
 test('should have content', () => {
+  useSocket.mockImplementation(() => ({
+    emit: () => {},
+    orders: {
+      basket: [{id: 1, name: 'chips', price: 200}],
+    },
+  }));
   const {getByTestId, getByText} = renderComponent();
   expect(getByText(frontmatter.title)).toBeDefined();
   expect(getByTestId('homeId')).toBeDefined();
 });
 
 test('should get basket orders', () => {
+  useSocket.mockImplementation(() => ({
+    emit: () => {},
+    orders: {
+      basket: [{id: 1, name: 'burger', price: 200}],
+    },
+  }));
   const {getByText, findByTestId, debug} = renderComponent();
   fireEvent.click(getByText(frontmatter.getOrders));
 });
